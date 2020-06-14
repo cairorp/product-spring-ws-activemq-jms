c2324812f8a4148bd4f4cf270d0f04712d72ff07
Cairo Pereira <cairo.151295@gmail.com>
2020-06-14 17:40:21 -0300

    Remover toString não necessário.

--- src/main/java/br/com/productspringwsactivemqjms/dto/ProductDto.java
+++ src/main/java/br/com/productspringwsactivemqjms/dto/ProductDto.java
@@ -2,11 +2,13 @@ package br.com.productspringwsactivemqjms.dto;
 
 import br.com.productspringwsactivemqjms.model.Product;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
 
 @Getter
+@ToString
 public class ProductDto implements Serializable {
     private String name;
     private BigDecimal price;
