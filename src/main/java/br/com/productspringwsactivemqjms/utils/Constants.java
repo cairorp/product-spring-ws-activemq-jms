8befeafcd5ca2aec9a9d4904e6782d342f6c009a
Cairo Pereira <cairo.151295@gmail.com>
2020-06-14 02:41:00 -0300

    Adicionar classe para constants.

---
+++ src/main/java/br/com/productspringwsactivemqjms/utils/Constants.java
@@ -0,0 +1,9 @@
+package br.com.productspringwsactivemqjms.utils;
+
+public class Constants {
+
+    public static final String PRODUCT_QUEUE = "queue.product";
+
+    private Constants() {
+    }
+}
