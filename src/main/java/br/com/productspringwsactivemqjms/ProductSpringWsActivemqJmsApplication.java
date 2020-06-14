31090956d6646d46b8dde78ef3307d3d55bafb0d
Cairo Pereira <cairo.151295@gmail.com>
2020-06-13 22:38:18 -0300

    Inicilizar projeto.

---
+++ src/main/java/br/com/productspringwsactivemqjms/ProductSpringWsActivemqJmsApplication.java
@@ -0,0 +1,13 @@
+package br.com.productspringwsactivemqjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSpringWsActivemqJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductSpringWsActivemqJmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/productspringwsactivemqjms/ProductSpringWsActivemqJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.productspringwsactivemqjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSpringWsActivemqJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
